ec6c992ac22133ce77516a41a4e892e6355b38ca
ebraheem <ebraheem20012020@gmail.com>
2023-01-29 19:37:35 +0200

    creating pages app

--- ems/settings.py
+++ ems/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ems.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASEDIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
