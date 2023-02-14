16173eb7a806647d1c869058ca79c4d56df810a7
ebraheem <ebraheem20012020@gmail.com>
2023-02-14 20:17:51 +0200

    just rewriting email validator

--- funcs/validators.py
+++ funcs/validators.py
@@ -1,7 +1,6 @@
 import re
 
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
 def validate_email(email):
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
     return re.fullmatch(regex, email)
