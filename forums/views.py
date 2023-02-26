429f6c8e2130588061fb36b70f305f7b5ff56d87
ebraheem <ebraheem20012020@gmail.com>
2023-02-26 21:12:59 +0200

    login required in forum views

--- forums/views.py
+++ forums/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 from .models import Article
 
-
+@login_required
 def article_list(request):
     articles = Article.objects.all()
-    context = {"articles": articles}
+    context = {
+        "articles": articles,
+    }
     return render(request, "forums/article_list.html", context)
 
-
-def article_details(request, id):
+@login_required
+def article_details(request, article_id):
     article = Article.objects.get(id=id)
-    context = {"article": article}
+    context = {
+        "article": article
+    }
     return render(request, "forums/article_detail.html", context)
